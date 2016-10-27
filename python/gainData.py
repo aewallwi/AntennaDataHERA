91063d432eb7af3c1971fccdc2680f94c50cfa34
Aaron Ewall-Wice <aaronew@dhcp-18-111-49-134.dyn.MIT.EDU>
2016-10-26 23:23:18 -0400

    finished gainData.py module and added plotting routine to compare time/frequency plots from Rich's measurements and CST simulations

--- python/gainData.py
+++ python/gainData.py
@@ -3,34 +3,56 @@
 #************************************************************
 import numpy as n
 import healpy as hp
+import numpy.fft as fft
+import scipy.signal as signal
 
-
-
-class metaData():
-    def __init__(self):
-        self.device=''
-        self.date=''
-        self.dtype=''
-        self.comment=''
-        self.date=''
-        self.datarange=''
+class MetaData():
+    def __init__(self,device='',date='',dtype='',comment='',datarange=[]):
+        self.device=device
+        self.date=date
+        self.dtype=dtype
+        self.comment=comment
+        self.datarange=datarange
 
 #Read HP VNA data used in Greenbank measurements
-def readVNAHP(filename,comment=''):
-    dataFile=open(filename)
+def readVNAHP(fileName,comment=''):
+    dataFile=open(fileName)
     dataLines=dataFile.readlines()
-    FLOW=float(dataLines[7].split()[1]);FHIGH=float(dataLines[7].split()[2]);NDATA=int(dataLines[7].split()[3])
-    data=n.loadtxt(filename,skiprows=9,delimiter=',')
-    device=dataLines[1]
+    FLOW=1e-9*float(dataLines[6].split()[1]);FHIGH=1e-9*float(dataLines[6].split()[2]);
+    NDATA=int(dataLines[6].split()[3])
+    data=n.loadtxt(fileName,skiprows=9,delimiter=',')
+    device=dataLines[1][:-2]
     dtype=dataLines[4].split()[1]
-    meta=metaData(device=device,dtype=['FREQ',dtype],datarange=[FLOW,FHIGH,NDATA],comment=comment)
-    fAxis=n.arange(NDATA)*(FHIGH-FLOW)+FLOW
+    meta=MetaData(device=device,dtype=['FREQ',dtype],datarange=[FLOW,FHIGH,NDATA],comment=comment)
+    fAxis=n.arange(NDATA)*(FHIGH-FLOW)/NDATA+FLOW
+    print fAxis
     return fAxis,data[:,0]+1j*data[:,1],meta
+
+#take ratio of fft of two inputs with padding
+def fftRatio(convolved,kernel,windowFunction):
+    nf=len(convolved)
+    if(windowFunction=='blackman-harris'):
+        wF=signal.blackmanharris(nf)
+    convolved_pad=n.pad(convolved,(nf/2,nf/2),mode='constant')
+    kernel_pad=n.pad(kernel,(nf/2,nf/2),mode='constant')
+    wF=n.pad(wF,(nf/2,nf/2),mode='constant')
+    return fft.fftshift(fft.fft(fft.fftshift(convolved_pad*wF)))/fft.fftshift(fft.fft(fft.fftshift(kernel_pad*wF)))
     
+
 #Read CST time trace file
-def readCSTTimeTrace(filename,comment=''):
-    dataFile=open(filename)
+def readCSTTimeTrace(fileName,comment=''):
+    dataFile=open(fileName)
     dataLines=dataFile.readlines()
+    header=dataLines[:2]
+    if('ns' in header[0]):
+        tFactor=1.
+    if('ms' in header[0]):
+        tFactor=1e6
+    if('micro' in header[0]):
+        tFactor=1e3
+    if('sec' in header[0]):
+        tFactor=1e9
+        
     inputTrace=[]
     outputTrace1=[]
     outputTrace2=[]
@@ -52,22 +74,69 @@ def readCSTTimeTrace(filename,comment=''):
             dtype='PlaneWave Excitation'
         else:
             entry=dataLines[lNum].split()
-            thisTrace.append([float(entry[0]),float(entry[1])])
+            if(len(entry)==2):
+                thisTrace.append([float(entry[0]),float(entry[1])])
             lNum+=1
     inputTrace=n.array(inputTrace)
     outputTrace1=n.array(outputTrace1)
     outputTrace2=n.array(outputTrace2)
-    meta=(device='CST',dtype=['Time',dtype],datarange=[inputTrace[:,0].min(),inputTrace[:,0].max(),len(inputTrace[:,0])],comment=comment)
+    inputTrace[:,0]*=tFactor
+    outputTrace1[:,1]*=tFactor
+    if(len(outputTrace2)>0):
+        outputTrace2[:,1]*=tFactor
+    meta=MetaData(device='CST',dtype=['TIME',dtype],datarange=[inputTrace[:,0].min(),inputTrace[:,0].max(),len(inputTrace[:,0])],comment=comment)
     return [inputTrace,outputTrace1,outputTrace2],meta
     
-def readCSTS11(filename,comment=''):
-    amp=n.loadtxt(filename+'_amp.txt',skiprows=2)
-    pha=n.loadtxt(filename+'_pha.txt',skiprows=2)
+def readCSTS11(fileName,comment='',degrees=True):
+    header=open(fileName+'_abs.txt').readlines()[:2]
+    if('MHz' in header[0]):
+        fFactor=1e-3
+    elif('GHz' in header[0]):
+        fFactor=1e0
+    elif('kHz' in header[0]):
+        fFactor=1e-6
+    elif('Hz' in header[0]):
+        fFactor=1e-9
+        
+    amp=n.loadtxt(fileName+'_abs.txt',skiprows=2)
+    fAxis=amp[:,0]
+    amp=amp[:,1]
+    pha=n.loadtxt(fileName+'_pha.txt',skiprows=2)[:,1]
+    if(degrees):
+        pha*=n.pi/180.
+    data=amp*n.exp(1j*pha)
+    meta=MetaData(device='CST',dtype=['FREQ','S11'],datarange=[fAxis.min(),fAxis.max(),len(fAxis)],comment=comment)
+    return fFactor*fAxis,data,meta
     
 
-FILETYPES=['CST','VNAHP']
-class gainData():
-    def __init__(self):
-        
+FILETYPES=['CST_TimeTrace','CST_S11','VNAHP_S11']
+class GainData():
+    def __init__(self,fileName,fileType,fMin=None,fMax=None,windowFunction=None,comment=''):
+        assert fileType in FILETYPES
+        if(windowFunction is None):
+            windowFunction = 'blackman-harris'
+        self.windowFunction=windowFunction
+        if (fileType=='CST_TimeTrace'):
+            [inputTrace,outputTrace,_],self.metaData=readCSTTimeTrace(fileName,comment=comment)
+            self.fAxis=fft.fftshift(fft.fftfreq(len(inputTrace)*2,inputTrace[1,0]-inputTrace[0,0]))
+            self.gainFrequency=fftRatio(outputTrace[:,1],inputTrace[:,1],windowFunction=windowFunction)
+            
+        elif(fileType=='CST_S11'):
+            self.fAxis,self.gainFrequency,self.metaData=readCSTS11(fileName,comment=comment)
+        elif(fileType=='VNAHP_S11'):
+            self.fAxis,self.gainFrequency,self.metaData=readVNAHP(fileName,comment=comment)
+        if(fMin is None):
+            fMin=self.fAxis.min()            
+        if(fMax is None):
+            fMax=self.fAxis.max()
+            
+            
+        selection=n.logical_and(self.fAxis>=fMin,self.fAxis<=fMax)
+        self.fAxis=self.fAxis[selection]
+        self.gainFrequency=self.gainFrequency[selection]
+        if(windowFunction== 'blackman-harris'):
+            wF=signal.blackmanharris(len(self.fAxis))
+        self.tAxis=fft.fftshift(fft.fftfreq(len(self.fAxis),self.fAxis[1]-self.fAxis[0]))
+        self.gainDelay=fft.fftshift(fft.ifft(fft.fftshift(self.gainFrequency*wF)))
         
     
---
+++ python/plotComparison_S11_TallCylinderGapFeedAndDish.py
@@ -0,0 +1,35 @@
+import numpy as n
+import matplotlib.pyplot as p
+import gainData as gainData
+
+fileNameTimeTraceCST='../cst/TallCylinderGapOverDish/TallCylinderGapOverDish_TerminalExcitation_timetrace.txt'
+fileNameS11VNA='../reflectometry/RichBradley_GreenBank/TallCylinderGapOverDish_S11_Greenbank_RichBradley.d1'
+fileNameS11CST='../cst/TallCylinderGapOverDish/TallCylinderGapOverDish_S11'
+
+FLOW=0.1
+FHIGH=0.2
+
+
+gainData_timeTrace=gainData.GainData(fileNameTimeTraceCST,
+                                     fileType='CST_TimeTrace',
+                                     fMin=FLOW,fMax=FHIGH,
+                                     comment='s11 derived from cst time domain data')
+gainData_cst=gainData.GainData(fileNameS11CST,
+                               fileType='CST_S11',
+                               fMin=FLOW,fMax=FHIGH,
+                               comment='s11 obtained directly from cst')
+gainData_vna=gainData.GainData(fileNameS11VNA,
+                               fileType='VNAHP_S11',
+                               fMin=FLOW,fMax=FHIGH,
+                               comment='s11 obtained from richs vna measurement')
+
+print gainData_vna.tAxis
+print gainData_timeTrace.tAxis
+print gainData_timeTrace.gainDelay
+
+#first make original plot comparing s11 of time trace and s11 of vna
+p.plot(gainData_vna.tAxis,10.*n.log10(n.abs(gainData_vna.gainDelay)),color='grey',ls='-',marker='o')
+p.plot(gainData_timeTrace.tAxis,10.*n.log10(n.abs(gainData_timeTrace.gainDelay)),color='k',ls='-',marker='o')
+p.xlim(-30,400)
+p.ylim(-50,0)
+p.show()
